3dd94ce4cc9e295d44c008186ed219ec5ca6fe6f
antz-H <huang7230468@163.com>
2019-11-14 23:19:07 +0800

    <style> modify gitigore

---
+++ Antz-Domain-Core-One/src/main/java/com/antz/domain/core/RandomIdentity.java
@@ -0,0 +1,11 @@
+package com.antz.domain.core;
+
+/**
+ * @author antz
+ * @description
+ * @date 2019/11/11
+ */
+public interface RandomIdentity<T> {
+
+    T next();
+}
---
+++ Antz-Tools/src/main/java/com/antz/tools/io/PropertiesTest.java
@@ -0,0 +1,48 @@
+package com.antz.tools.io;
+
+import java.io.*;
+import java.time.LocalDate;
+import java.util.Properties;
+import java.util.UUID;
+
+/**
+ * @author antz
+ * @description
+ * @date 2019/11/14
+ */
+public class PropertiesTest {
+
+    public String loadProperties(String filePath, String key) {
+        Properties properties = new Properties();
+        try {
+            InputStream in = new BufferedInputStream(new FileInputStream(filePath));
+            properties.load(in);
+            String value = properties.getProperty(key);
+            return value;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void saveProperties(String filePath, String key, String value) throws IOException {
+        Properties pps = new Properties();
+        InputStream in = new FileInputStream(filePath);
+        pps.load(in);
+        OutputStream out = new FileOutputStream(filePath);
+        pps.setProperty(key, value);
+        //实时追加到文件末梢
+        pps.store(out, "Update " + key);
+    }
+
+    public static void main(String[] args) throws IOException {
+        PropertiesTest propertiesTest = new PropertiesTest();
+        propertiesTest.saveProperties("C:\\home\\pps\\test.instance", "instanceId", UUID.randomUUID().toString());
+        propertiesTest.saveProperties("C:\\home\\pps\\test.instance", "startTime", LocalDate.now().toString());
+        String instanceId = propertiesTest.loadProperties("C:\\home\\pps\\test.instance", "instanceId");
+        String startTime = propertiesTest.loadProperties("C:\\home\\pps\\test.instance", "startTime");
+        System.out.println(instanceId);
+        System.out.println(startTime);
+    }
+
+}
