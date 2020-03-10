3fc9a4618ed492b9565840548ff022fd6432a66a
huanghuang <huang7230468@163.com>
2020-03-10 23:41:18 +0800

    <refactor> delete useless id for ID

--- Antz-Domain-Core-TWO/src/main/java/com/antz/domain/core/ID.java
+++ Antz-Domain-Core-TWO/src/main/java/com/antz/domain/core/ID.java
@@ -1,7 +1,6 @@
 package com.antz.domain.core;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 /**
  * @author Antz-H
@@ -11,22 +10,5 @@ import java.util.Objects;
 public abstract class ID implements Serializable {
 
     private static final long serialVersionUID = -6936162521699449455L;
-    private String id;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        ID id1 = (ID) o;
-        return Objects.equals(id, id1.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
