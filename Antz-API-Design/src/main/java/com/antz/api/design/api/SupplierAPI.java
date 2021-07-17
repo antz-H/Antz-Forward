b43b6624471480247f62e957179204badd6fd481
huang.huang <huang.huang@derbysoft.net>
2021-07-17 21:20:57 +0800

    add design pattern module

--- Antz-API-Design/src/main/java/com/antz/api/design/api/SupplierAPI.java
+++ Antz-API-Design/src/main/java/com/antz/api/design/api/SupplierAPI.java
@@ -1,5 +1,8 @@
 package com.antz.api.design.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author antz-H
  * @description
